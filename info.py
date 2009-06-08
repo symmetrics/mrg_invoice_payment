bb4a9d753d56947b79e046892e7f616d9d2cbbae
saasrepo <saasrepo@saasrepo.symmetrics.de>
2009-06-08 18:30:28 +0200

    Initial import

---
+++ info.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+
+
+# =============================================================================
+# package info
+# =============================================================================
+NAME = 'symmetrics_module_invoice_payment'
+
+TAGS = ()
+
+LICENSE = 'AFL 3.0'
+
+HOMEPAGE = 'http://www.symmetrics.de'
+
+INSTALL_PATH = ''
+
+
+# =============================================================================
+# responsibilities
+# =============================================================================
+TEAM_LEADER = {
+    '[firstname] [lastname]': '[email]'
+}
+
+MAINTAINER = {
+    '[firstname] [lastname]': '[email]'
+}
+
+AUTHORS = {
+    '[firstname] [lastname]': '[email]',
+}
+
+# =============================================================================
+# additional infos
+# =============================================================================
+INFO = '[short description, max 60 chars]'
+
+SUMMARY = '''
+    [long description
+    with multiple lines]
+'''
+
+NOTES = '''
+    [special notes, restrictions, bugs etc.]
+'''
+
+# =============================================================================
+# relations
+# =============================================================================
+REQUIRES = {
+}
+
+EXCLUDES = {
+}
+
+DEPENDS_ON_FILES = (
+    'app/code/core/.../1.php',
+    'app/code/core/.../2.php',
+    'app/code/core/.../3.php',
+)
+
+PEAR_KEY = ''
+
+COMPATIBLE_WITH = {
+}
