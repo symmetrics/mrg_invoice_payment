9a9e3bd3b549a795787d0aa7c018baef8bc2ae45
Eric Reiche <er@symmetrics.de>
2009-10-28 15:32:05 +0100

    Readme, info.py

--- info.py
+++ info.py
@@ -59,6 +59,6 @@ DEPENDS_ON_FILES = ()
 PEAR_KEY = ''
 
 COMPATIBLE_WITH = {
-    'magento': ['1.3.2.3'],
-	'magento_enterprise': ['1.3.2.4']
+    'magento': ['1.3.2.3', '1.3.2.4'],
+	'magento_enterprise': ['1.3.2.3', '1.3.2.4']
 }
